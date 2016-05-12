77880a5ee60219e82ece7e5ba4d1cbf07902ce8a
Romain ACCIARI <romain.acciari@gmail.com>
2016-05-12 20:38:06 +0200

    Update for 16.04

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,57 +1,74 @@
-openiosds::sdsagent {'sds-agent-0':
-  no_exec => true,
-}
 class {'gridinit':
   no_exec => true,
 }
+class{'openiosds':}
 openiosds::namespace {'OPENIO':
   ns             => 'OPENIO',
   conscience_url => '127.0.0.1:6000',
-  eventagent_url => 'tcp://127.0.0.1:6008',
   oioproxy_url   => '127.0.0.1:6006',
+  eventagent_url => 'beanstalk://127.0.0.1:6014',
 }
-openiosds::conscience {'conscience-1':
+openiosds::account {'account-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::meta0 {'meta0-1':
+openiosds::conscience {'conscience-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::meta1 {'meta1-1':
+openiosds::meta0 {'meta0-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::meta2 {'meta2-1':
+openiosds::meta1 {'meta1-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::rawx {'rawx-1':
+openiosds::meta2 {'meta2-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::oioeventagent {'oio-event-agent-1':
+openiosds::rawx {'rawx-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
 }
-openiosds::account {'account-1':
-  ns         => 'OPENIO',
-  ipaddress  => '127.0.0.1',
-  redis_host => '127.0.0.1',
-  no_exec    => true,
+openiosds::rdir {'rdir-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
 }
-openiosds::redis {'redis-1':
-  ns         => 'OPENIO',
-  ipaddress  => '127.0.0.1',
-  no_exec    => true,
+openiosds::oioblobindexer {'oio-blob-indexer-rawx-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
 }
-openiosds::oioproxy {'oioproxy-1':
+openiosds::oioeventagent {'oio-event-agent-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
+}
+openiosds::oioproxy {'oioproxy-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
+}
+openiosds::redis {'redis-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
+}
+openiosds::conscienceagent {'conscienceagent-0':
+  ns        => 'OPENIO',
+  ipaddress => '127.0.0.1',
+  no_exec   => true,
+}
+openiosds::beanstalkd {'beanstalkd-0':
   ns        => 'OPENIO',
   ipaddress => '127.0.0.1',
   no_exec   => true,
